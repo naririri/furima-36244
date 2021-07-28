0c2957237c49ff76c5397c5f7237a221913b45d6
naririri <nanananaririri@gmail.com>
2021-07-28 18:22:31 +0900

    新規登録・ログインの挙動実装（テーブル保存）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  #before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -8,4 +10,9 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :email, :password, :last_name, :first_name, :reading_last_name, :reading_first_name, :birthday])
+  end
+
 end
