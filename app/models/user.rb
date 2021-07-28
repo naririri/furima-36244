244f29c730f32c5b0cea5dd0492022a1804ea4ff
naririri <nanananaririri@gmail.com>
2021-07-28 19:27:23 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password,             format: {with: VALID_PASSWORD_REGEX, message: "is invalid. Include both letters and numbers"}
+  with_options presence: true do
+    validates :nick_name
+    validates :last_name,          format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters"}
+    validates :first_name,         format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters"}
+    validates :reading_last_name,  format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters"}
+    validates :reading_first_name, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters"}
+    validates :birthday
+  end
 end
