cd67da10a708d5564d370033365845f0165f20a5
naririri <nanananaririri@gmail.com>
2021-07-30 17:44:07 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,24 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
+  belongs_to :category_id
+  belongs_to :condition_id
+  belongs_to :postage_id
+  belongs_to :prefecture_id
+  belongs_to :post_date_id
+
+  with_options presence: true do
+    validates :name
+    validates :info
+    validates :price
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :postage_id
+    validates :prefecture_id
+    validates :post_date_id
+  end
 end
