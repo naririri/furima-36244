aa3efec978972876f9e96e21536a382938043334
naririri <nanananaririri@gmail.com>
2021-07-30 16:39:22 +0900

    アソシエーション（itemモデル）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,12 @@ class ItemsController < ApplicationController
   def index
     # current_user = User.find(params[:id])
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :info, :category_id, :condition_id, :postage_id, :prefecture_id, :post_date_id, :price).merge(:user_id: current_user.id)
+  end
+
+
 end
