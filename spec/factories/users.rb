49bfae25cdca9ee6a1fbe7f56106e948c5fbfc21
naririri <nanananaririri@gmail.com>
2021-08-02 15:53:42 +0900

    Issue5再々々提出

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     # password { 'test1234' }
     nick_name             { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     last_name { '手口' }
     first_name { '脚譜男' }
