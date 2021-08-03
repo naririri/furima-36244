5093ecc8e9639edc1c5d169124700c56686c125e
naririri <nanananaririri@gmail.com>
2021-08-03 13:11:24 +0900

    編集画面の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -21,6 +21,12 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(params[:id])  
+  end
+
+
 
   private
 
