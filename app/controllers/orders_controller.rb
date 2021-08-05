ce6fa91f0c44ad9b2f5812733647c0845aeaaa5b
naririri <nanananaririri@gmail.com>
2021-08-05 23:35:57 +0900

    SOLD OUTの表示と遷移の制限設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,19 @@
 class OrdersController < ApplicationController
+before_action :authenticate_user!, only: [:index, :create]
 
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
-    #@order_address = OrderAddress.new(params[:item_id])
+    if (current_user.id == @item.user_id) || @item.order.present?
+      redirect_to root_path
+    end
   end
 
   def create
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      card_pay
       @order_address.save
       redirect_to root_path
     else
@@ -20,11 +24,16 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_number, :prefecture_id, :city, :house_number, :building, :mobile_number).merge(user_id: current_user.id,item_id: params[:item_id])
-    #params.permit(:postal_number, :prefecture_id, :city, :house_number, :building, :mobile_number).merge(user_id: current_user.id,item_id: params[:item_id])
+    params.require(:order_address).permit(:postal_number, :prefecture_id, :city, :house_number, :building, :mobile_number).merge(user_id: current_user.id,item_id: params[:item_id],token: params[:token])
   end
 
-  #def address_params
-    #params.permit(:postal_number, :prefecture_id, :city, :house_number, :building, :mobile_number).merge(user_id: current_user.id,item_id: params[:item_id])
-  #end
+  def card_pay
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
+  end
+
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,13 +1,13 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_number, :prefecture_id, :city, :house_number, :building, :mobile_number, :user_id, :item_id, :order_id
+  attr_accessor :postal_number, :prefecture_id, :city, :house_number, :building, :mobile_number, :user_id, :item_id, :order_id, :token
 
   with_options presence: true do
     validates :city
     validates :house_number
     validates :user_id
     validates :item_id
-    validates :order_id
+    validates :token
   end
 
   validates :mobile_number, presence: true, format: { with: /\A\d{10,11}\z/}
@@ -16,8 +16,8 @@ class OrderAddress
 
 
   def save
-    @order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_number: postal_number, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, mobile_number: mobile_number, order_id: @order.id)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_number: postal_number, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, mobile_number: mobile_number, order_id: order.id)
     #Address.create(postal_number: postal_number, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, mobile_number: mobile_number, user_id: user_id, item_id: item_id)
   end
 end
